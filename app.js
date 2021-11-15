671f261b2b3f16cb8c3a2ccda2c2587d0190724b
NoelanL <92019519+NoelanL@users.noreply.github.com>
2021-11-15 10:27:26 +0100

    Faire exercice 56!

---
+++ app.js
@@ -0,0 +1,30 @@
+let maVariable = prompt("Tapez un accessoire informatique en minuscule !");
+
+switch(maVariable){
+    case 'souris':
+        alert("J'utilise une souris");
+        break;
+
+    case 'clavier':
+        alert("J'utilise un clavier");
+        break;
+
+    case 'ordinateur':
+        alert("J'utilise un ordinateur");
+        break;
+
+    case 'tapis':
+        alert("J'utilise un tapis");
+        break;
+
+    case 'imprimante':
+        alert("J'utilise une imprimante")
+        break;
+
+    case 'ondulateur':
+        alert("J'utilise un ondulateur");
+        break;
+
+    default:
+        alert("L'accessoire est inconnu !")
+}
